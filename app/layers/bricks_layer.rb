2c34476bf87d05f3adb055ef493eb72c6da3148b
Mathieu Gagné <gagne.mathieu@hotmail.com>
2020-06-14 02:07:29 -0400

    Add bricks (#24)
    * Renames model length to width
    
    * Adds bricks

--- app/layers/bricks_layer.rb
+++ app/layers/bricks_layer.rb
@@ -1,8 +1,9 @@
 class BricksLayer
   class << self
-    def render(bricks)
-      bricks.each do |brick|
+    def render(brick_layout)
+      brick_layout.bricks.each do |brick|
         $args.outputs.solids << [brick.x, brick.y, brick.width, brick.height, *brick.color]
+        $args.outputs.borders << [brick.x, brick.y, brick.width, brick.height, *brick.border]
       end
     end
   end
--- app/layers/paddle_layer.rb
+++ app/layers/paddle_layer.rb
@@ -1,7 +1,7 @@
 class PaddleLayer
   class << self
     def render(paddle)
-      $args.outputs.solids << [paddle.x, paddle.y, paddle.length, paddle.height, *paddle.color]
+      $args.outputs.solids << [paddle.x, paddle.y, paddle.width, paddle.height, *paddle.color]
     end
   end
 end
