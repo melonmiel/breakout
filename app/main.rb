db5c6d7c95280ef009638886e67d867eef029000
Mathieu Gagné <gagne.mathieu@hotmail.com>
2020-06-13 22:47:25 -0400

    Adds Game, Level & Paddle

--- app/main.rb
+++ app/main.rb
@@ -1,4 +1,7 @@
-def tick args
-  args.outputs.labels << [580, 460, 'Hello Sexy Beast!', 255, 255, 255 ]
-  args.outputs.solids << [0, 0, 1280, 720]
+require "lib/autoload.rb"
+
+$game = GameController.new
+
+def tick(args)
+  $game.tick(args)
 end
