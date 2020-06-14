1e299cf87c58d877b91467554d3c905f7f05d88b
Mathieu Gagné <gagne.mathieu@hotmail.com>
2020-06-14 12:59:35 -0400

    Better menu color

--- app/models/menu_item.rb
+++ app/models/menu_item.rb
@@ -29,7 +29,7 @@ class MenuItem < Engine::Model
   end
 
   def set_color
-    @color = selected ? [*ColorPalette.purple, alpha] : ColorPalette.text
+    @color = selected ? [*ColorPalette.cyan, alpha] : ColorPalette.text
   end
 
   def alpha
