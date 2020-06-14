66e1798b3f6da7bf0fcd309edd5598c5d40a76a6
Mathieu Gagné <gagne.mathieu@hotmail.com>
2020-06-14 02:07:29 -0400

    Add bricks (#24)
    * Renames model length to width
    
    * Adds bricks

--- app/models/brick.rb
+++ app/models/brick.rb
@@ -1,2 +1,16 @@
 class Brick < Models::Base
+  WIDTH = 75
+  HEIGHT = 40
+  BRICK_COLOR = [255, 255, 255].freeze
+  BORDER_COLOR = [0, 0, 0].freeze
+  PADDING = 5
+
+  def initialize(x, y)
+    @x = x
+    @y = y
+    @width = WIDTH
+    @height = HEIGHT
+    @color = BRICK_COLOR
+    @border = BORDER_COLOR
+  end
 end
---
+++ app/models/brick_layout.rb
@@ -0,0 +1,29 @@
+class BrickLayout < Models::Base
+  PADDING_TOP = 100
+
+  attr_accessor :rows, :columns, :bricks
+
+  def initialize(rows:, columns:)
+    @rows = rows
+    @columns = columns
+    @bricks = setup_bricks
+  end
+
+  def setup_bricks
+    columns.times.flat_map do |column|
+      rows.times.map do |row|
+        x = left_edge + (column * Brick::WIDTH)
+        y = top_edge - (row * Brick::HEIGHT)
+        Brick.new(x, y)
+      end
+    end
+  end
+
+  def left_edge
+    @left_edge ||= Viewport.center(columns * Brick::WIDTH)
+  end
+
+  def top_edge
+    @top_edge ||= Viewport.height - PADDING_TOP
+  end
+end
