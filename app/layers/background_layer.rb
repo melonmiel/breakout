b12d7331c085c86a29200eff1adeedb09956cac6
Mathieu Gagné <gagne.mathieu@hotmail.com>
2020-06-14 11:35:41 -0400

    Adds ColorPalette

--- app/layers/background_layer.rb
+++ app/layers/background_layer.rb
@@ -1,7 +1,7 @@
 class BackgroundLayer
   class << self
     def render
-      $args.outputs.solids << [0, 0, 1280, 720]
+      $args.outputs.solids << [0, 0, 1280, 720, *ColorPalette.background]
     end
   end
 end
