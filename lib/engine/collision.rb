b7a003ae984c7d584184cd5a6a94a3cf954b7104
Mathieu Gagné <gagne.mathieu@hotmail.com>
2020-06-15 22:26:26 -0400

    Uses GTK::Geometry class method

--- lib/engine/collision.rb
+++ lib/engine/collision.rb
@@ -2,7 +2,7 @@ module Engine
   module Collision
     def on_collision(source, destinations, &block)
       destinations.each do |destination|
-        next unless source.rect.intersect_rect?(destination.rect)
+        next unless GTK::Geometry.intersect_rect?(source.rect, destination.rect)
 
         yield(destination)
       end
