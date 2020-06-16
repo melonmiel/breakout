43b1292f2d18c13552236ab994b5a98f6ef3ff2b
Mathieu Gagné <gagne.mathieu@hotmail.com>
2020-06-16 00:08:47 -0400

    Adds Model#rect

--- lib/engine/model.rb
+++ lib/engine/model.rb
@@ -2,12 +2,23 @@ module Engine
   class Model
     attr_accessor :x, :y, :width, :height, :color, :border, :alpha, :font
 
+    def initialize(x: nil, y: nil, width: nil, height: nil)
+      @x = x
+      @y = y
+      @width = width
+      @height = height
+    end
+
     def alpha
       @alpha || 255
     end
 
     def font
       @font || "app/assets/fonts/VerminVibes1989Regular.ttf"
+    end
+
+    def rect
+      [x, y, width, height]
     end
   end
 end
