e0eb4c55d3fd3db6645592eef57d58224ab0cb14
Mathieu Gagné <gagne.mathieu@hotmail.com>
2020-06-21 19:01:59 -0400

    Adds framerate in top-left corner

--- app/main.rb
+++ app/main.rb
@@ -1,8 +1,10 @@
 require "lib/autoload.rb"
 
-$gtk.reset
-
 def tick(args)
   $args = args
   GameController.tick
+
+  # TODO: Work with environment variables
+  # TODO: Display only if developping
+  $args.outputs.labels << [10, 710, args.gtk.current_framerate, 255, 255, 255]
 end
