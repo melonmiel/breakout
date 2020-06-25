9bfdeeeb5d728a654d1ed20700bd0488113b7c6c
Mathieu Gagné <gagne.mathieu@hotmail.com>
2020-06-24 20:05:01 -0400

    Adds assets helpers

--- lib/engine/concerns/assets.rb
+++ lib/engine/concerns/assets.rb
@@ -8,8 +8,20 @@ module Engine
       "app/assets/sounds/#{filename}"
     end
 
-    def play_sound(reference)
-      $args.outputs.sounds << sound_path(reference)
+    def song_path(filename)
+      "app/assets/songs/#{filename}"
+    end
+
+    def play_sound(sound)
+      $args.outputs.sounds << sound_path(sound)
+    end
+
+    def play_music(song)
+      $args.outputs.sounds << sound_path(song)
+    end
+
+    def stop_music
+      $args.outputs.sounds << sound_path("silence.ogg")
     end
   end
 end
